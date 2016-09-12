a0892d41dee5862d2f34ac55243718d1f13707b9
saurabh <sr449386@dal.ca>
2016-09-12 13:32:50 -0300

    test commit

---
+++ Datatypeextracter.h
@@ -0,0 +1,13 @@
+void DataTypeExtracter()
+{
+TypeRepository type_object;
+TypeId type_id_typerepo = data().type_id();  // neded to figure out if this is a right way to get typeid from the Typed block layer
+TypePtr  type_pointer = type_object.GetType(&type_id_typerepo); // @p type_pointer a TypePtr pointer to point to Type class 
+
+//here is the place which need to be figure out how to down cast the type we get. 
+
+if (*type_pointer=USER_DEFINED_TYPE_KIND)
+{
+
+}
+};
