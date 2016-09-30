16ee56eb6eef561eaa2942b8b602428ce85ccd65
saurasingh <saurabhsingh14sep@atlassian.com>
2016-09-30 01:49:38 -0300

    new unordered map

--- unordered_mapping.cc
+++ unordered_mapping.cc
@@ -1,12 +1,18 @@
+struct Vertex
+{
+	Adress Adress;
+	size_t size;
+	Type DataType;
+	
+}
+
 struct Key
 {
-  Address Address_starting;
-  Address Address_ending;
+  Address Address;
   size_t size;
 
   bool operator==(const Key &other) const
-  { return (Address_starting == other.Address_starting
-            && Address_ending == other.Address_ending
+  { return (Address  == other.Address 
             && size == other.size);
   }
 };
@@ -21,11 +27,22 @@ template <>
             // Compute individual hash values for Address_starting, Address_ending and size 
             // http://stackoverflow.com/a/1646913/126995
             size_t res = 1009;
-            res = res * 9176 + hash<string>()( k.Address_starting );
-            res = res * 9176 + hash<string>()( k.Address_ending );
-            res = res * 9176 + hash<int>()( k.size );
+            res = res * 9176 + hash<Address>()( k.Address );
+            res = res * 9176 + hash<size_t>()( k.size );
             return res;
         }
     };
 
-Vertex *
+Vertex *FindorCreatevertex(Adress, Type,hash,Key)
+{
+    	unordered_map<Address, Type, hash, > vertex_map;
+		if (vertex_map.find(Key)==vertex_map.end())
+		{
+			vertex_map.insert(make_pair(Key,Type);
+		}
+		else
+		{
+			return vertex_map.find(Key);
+		}
+	
+	}
