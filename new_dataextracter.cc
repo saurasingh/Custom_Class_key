803bfe38b59ff9c5cd3ed592d6a4e4f7ded9d5f0
saurasingh <saurabhsingh14sep@atlassian.com>
2016-10-04 12:38:36 -0300

    updated new_dataextracter

--- new_dataextracter.cc
+++ new_dataextracter.cc
@@ -1,11 +1,13 @@
 #include "syzygy\refinery\types\typed_data.h"
 #include "syzygy\refinery\types\typed_data.c"
+#include "syzygy\refinery\types\type.c"
+#include "syzygy\refinery\types\type.h"
 
 void Data_Extracter()
 {
 
 
-
+Type get_type_name;
 TypePtr data_type;
 Address address;
 size_t bit_position;
@@ -18,7 +20,10 @@ bit_position = typedata_object.bit_pos();
 bit_length = typedata_object.bit_len();
 
 if (data_type == BASIC_TYPE_KIND )
-
+	
+	string16 type_name= get_type_name.GetName(); //what I should pass here to get the name... how my program will know what data type to return.
+	Vertex(address,type_name,bit_length);
 
 
 }
+elseif (data_type== Pointer)
